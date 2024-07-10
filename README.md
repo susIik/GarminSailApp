3ab04ad5b7de1ac86b15c33c67ae7689d8b2ef1e
susIik <tcsuslik@gmail.com>
2024-07-10 14:59:34 +0300

    add auto build for all devices option

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+# Sail app
+
 This is a Sailing app for garmin devices.
+
+## Installation to device
+
+- Find the right version of the app from build folder
+- Connect your watch with computer using the data cable
+- Copy `.prg` file to your watch folder `GARMIN\APPS`
+- Now you can disconnect your watch from computer and you are ready to go
---
+++ build.py
@@ -0,0 +1,25 @@
+import subprocess
+import configparser
+
+
+def Build():
+    data = ReadCfg()
+
+    for device in data["devices"]:
+        out_path = f"./build/{device}/{data['name']}-{device}.prg"
+        #print(out_path)
+        subprocess.run(["monkeyc", "-o", out_path, "-d", device, "-f", data["jungle"], "-y", data["key"]], shell=True)
+
+
+def ReadCfg():
+    conf = configparser.ConfigParser()
+    conf.read("build.cfg")
+
+    return {"name": conf.get("DEFAULT", "app_name"),
+            "devices": conf.get("DEFAULT", "devices").strip().split(", "),
+            "key": conf.get("DEFAULT", "key"),
+            "jungle": conf.get("DEFAULT", "jungle"),}
+
+
+if __name__ == "__main__":
+    Build()
